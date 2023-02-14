46289e74d9235bc0776cbe8712676926e7aac180
bolatitomary <bolatito14@gmail.com>
2023-02-14 04:18:13 +0100

    new changes

--- JS/index.js
+++ JS/index.js
@@ -2,11 +2,11 @@ const navbar = document.querySelector("nav");
 const openMenu = document.getElementById("menu-button");
 const closeMenu = document.getElementById("menu-close");
 
-openMenu.addEventListener("click" , ()=>{
+openMenu.addEventListener("click" , () => {
     navbar.classList.add("open");
 });
 
-closeMenu.addEventListener("click" , ()=>{
+closeMenu.addEventListener("click" , () => {
     navbar.classList.remove("open");
 });
 
